8b74a5d74bbf13cccd8c7d08eff3f28bc2f3cd3f
AliBabr <38595649+AliBabr@users.noreply.github.com>
2019-10-20 03:16:05 +0500

    mail issue testing (#4)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 587,
+    port:                 25,
     domain:               'gmail.com',
     user_name:            'spur10.system@gmail.com',
     password:             'spur12345678',
