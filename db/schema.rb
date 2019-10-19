0ab5128ba73f55c31ecc69bd795d0aa79d0d2160
Ali Waqar <aliwaqar981@gmail.com>
2019-10-19 16:51:48 +0500

    integrated google-places api

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_10_18_221100) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
