a55cb9c5a5f1b8a52e783462def745901daa7c58
alibabr <babar2166@gmail.com>
2019-10-20 03:46:31 +0500

    mailer

--- config/environment.rb
+++ config/environment.rb
@@ -1,24 +1,24 @@
 # Load the Rails application.
 require_relative 'application'
 
-
-
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.perform_caching = false
-# config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-config.action_mailer.default_url_options = { host: 'https://spur10.herokuapp.com' }
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 25,
-  domain:               'gmail.com',
-  user_name:            'spur10.system@gmail.com',
-  password:             'spur12345678',
-  authentication:       'plain'
-  # enable_starttls_auto: true
-  # ^ ^ remove this option ^ ^
-}
-
-
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+Rails.application.configure do
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://spur10.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 25,
+    domain:               'gmail.com',
+    user_name:            'spur10.system@gmail.com',
+    password:             'spur12345678',
+    authentication:       'plain'
+    # enable_starttls_auto: true
+    # ^ ^ remove this option ^ ^
+  }
+end
