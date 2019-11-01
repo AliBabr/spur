973eb56cc114c7b9b1eada8de28813c189b06e9b
alibabr <babar2166@gmail.com>
2019-11-02 02:57:41 +0500

    fixed mail isuue and lint code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
 
@@ -5,14 +7,13 @@ class ApplicationController < ActionController::Base
   def authenticate
     @user = User.find_by_id(request.headers['X-SPUR-USER-ID'])
     if @user.present?
-      if User.validate_token(request.headers['X-SPUR-USER-ID'],request.headers['Authentication-Token'])
+      if User.validate_token(request.headers['X-SPUR-USER-ID'], request.headers['Authentication-Token'])
         return true
       else
-        render json: {message: "Unauthorized!"}, :status => 401
+        render json: { message: 'Unauthorized!' }, status: 401
       end
     else
-      render json: {message: "User Not Found!"}, :status => 404
+      render json: { message: 'User Not Found!' }, status: 404
     end
   end
-
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,12 @@
-class UserMailer < ApplicationMailer
+# frozen_string_literal: true
 
-  default from: "spur@gmail.com"
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.forgot_password.subject
-  #
+class UserMailer < ApplicationMailer
+  default from: 'spur@gmail.com'
+  #Makin email
   def forgot_password(user, token)
     @user = user
     @token = token
     @url = reset_api_v1_user_url(@user.id, tokens: token)
-    # @token = (0...50).map { ('a'..'z').to_a[rand(26)] }.join
-    mail to: user.email,  subject: "Forgot passowrd"
+    mail to: user.email, subject: 'Forgot passowrd'
   end
 end
