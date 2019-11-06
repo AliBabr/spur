93d04b0a1439d8ca718520b3b14fbeb27531e908
Ali Waqar <aliwaqar981@gmail.com>
2019-11-06 23:52:56 +0500

    Check added on places

--- app/services/places.rb
+++ app/services/places.rb
@@ -12,17 +12,19 @@ class Places
   def get_place
     selectPlace = []
     @price_level = @params[:price_level] if @params[:price_level].present?
-    if @params[:lat].present? && @params[:lng].present?
-      places=search_place
-      # Select places on the basis of price_level
-      places.each do |place|
-        selectPlace << place if place.price_level.present? && place.price_level == @price_level.to_i
+    places=search_place
+    if places.present?
+      if @params[:lat].present? && @params[:lng].present?
+        # Select places on the basis of price_level
+        places.each do |place|
+          selectPlace << place if place.price_level.present? && place.price_level == @price_level.to_i
+        end
+      else
+        return {:data => 'Longitude or Latitude missing', :message => 'Error', :status => 400}
       end
-    else
-      return {:data => 'Longitude or Latitude missing', :message => 'Error', :status => 400}
+      
+      place=choose_place(selectPlace)
     end
-    
-    place=choose_place(selectPlace)
     place.present? ? {:data => place, :message => 'Success', :status => 200}: {:data => "Broaden your search criteria, there were not enough available venues", :message => "Error", :status => 404}
   end
 
@@ -33,12 +35,11 @@ class Places
 
     @radius = @params[:radius] if @params[:radius].present?
     @type = @params[:type] if @params[:type].present?
-
     if @params[:filters].present?
       places = @client.spots(@params[:lat], @params[:lng], radius: @radius, type: @type, name: @params[:filters].values)
     else
       places = @client.spots(@params[:lat], @params[:lng], radius: @radius, type: @type)
-    end
+    end 
   end
 
   # Choose a place From searched places
