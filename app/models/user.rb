db1581bdfb0b7c26302d95d9212183ba7ee90900
alibabr <babar2166@gmail.com>
2019-11-20 23:33:05 +0500

    added profile photo for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :histories, dependent: :destroy
+  has_one_attached :profile_photo
   has_many :preferences, dependent: :destroy
 
 
